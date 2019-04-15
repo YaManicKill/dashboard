d105b21e4082cc8a28a4ed642e84d6905bad0f2b
Alistair McKinlay <github@10people.co.uk>
2019-04-15 11:16:44 +0100

    Departure board code

---
+++ src/dashboard/components/trains.vue
@@ -0,0 +1,13 @@
+var Rail = require("national-rail-darwin");
+var rail = new Rail("1d07b96d-3035-4ece-a643-3e4564df35c2");
+
+rail.getDepartureBoardWithDetails("HYM", {destination: "INK"}, (err, {trainServices}) => {
+    if (err) {
+        console.log("Err");
+        console.log(err);
+    } else {
+        trainServices.forEach((train) => {
+            console.log(`${train.std}: ${train.etd} (platform: ${train.platform}, Carriages: ${train.length}, To: ${train.destination.name})`);
+        });
+    }
+});
